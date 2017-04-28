1afe47b946e136bb9ade7b93c5351dee9b8cdc41
pkivolowitz <perry@kivolowitz.com>
2017-04-27 21:57:35 -0500

    Initial load

---
+++ client.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <memory.h>
+#include <getopt.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+
+using namespace std;
+
+bool keep_going = true;
+
+struct MyException
+{
+	MyException(string f, int l, string m) : function(string(f)), line(l), msg(m) {};
+	string function;
+	int line;
+	string msg;
+};
+
+#define MYEXCEPTION(m)		MyException(string(__FUNCTION__), __LINE__, string(m))
+
+void SIGINTHandler(int)
+{
+    keep_going = false;
+}
+
+int InitializeNetworkConnection(int argc, char * argv[])
+{
+	int server_socket = -1;
+	int port = 5077;
+	char * ip = (char *) ("127.0.0.1");
+	int opt;
+
+	while ((opt = getopt(argc, argv, "h:p:")) != -1) 
+	{
+		switch (opt) 
+		{
+			case 'h':
+				ip = optarg;
+				break;
+
+			case 'p':
+				port = atoi(optarg);
+				break;
+		}
+	}
+
+	server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_socket < 0)
+		throw MYEXCEPTION(string(strerror(errno)));
+
+	hostent * server_hostent = gethostbyname(ip);
+	if (server_hostent == nullptr) {
+		close(server_socket);
+		throw  MYEXCEPTION("Failed gethostbyname()");
+	}
+
+	sockaddr_in server_sockaddr;
+	memset(&server_sockaddr, 0, sizeof(sockaddr_in));
+	server_sockaddr.sin_family = AF_INET;
+	memmove(&server_sockaddr.sin_addr.s_addr, server_hostent->h_addr, server_hostent->h_length);
+	server_sockaddr.sin_port = htons(port);
+
+	if (connect(server_socket, (struct sockaddr*) &server_sockaddr, sizeof(sockaddr_in)) == -1)
+	{
+		throw MYEXCEPTION(string(strerror(errno)));
+	}
+
+	return server_socket;
+}
+
+void Send(int server_socket, string l)
+{
+	size_t length = l.size();
+	size_t bytes_sent;
+	size_t wire_length = htonl(length);
+
+	bytes_sent = send(server_socket, (void *) &wire_length, sizeof(wire_length), 0);
+	if (bytes_sent != sizeof(wire_length))
+		throw MYEXCEPTION("sending length of string");
+
+	bytes_sent = send(server_socket, (void *) &l[0], length, 0);
+	if (bytes_sent != length)
+		throw MYEXCEPTION("sending string");
+}
+
+int main(int argc, char * argv[])
+{
+	int server_socket = -1;
+	int rv = 0;
+	string l;
+
+	if (signal(SIGINT, SIGINTHandler) == SIG_ERR) {
+		cerr << "Setting signal failed." << endl;
+		return 1;
+	}
+
+	try {
+		server_socket = InitializeNetworkConnection(argc, argv);
+		while (keep_going) {
+			cout << "> ";
+			getline(cin, l);
+			Send(server_socket, l);
+		}
+	}
+	catch (MyException e) {
+		cerr << e.function << " " << e.line << " " << e.msg << endl;	
+		rv = 1;
+	}
+
+	if (server_socket >= 0)
+		close(server_socket);
+
+	return rv;
+}
---
+++ server.cpp
@@ -0,0 +1,157 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <thread>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <arpa/inet.h>
+#include <getopt.h>
+
+using namespace std;
+
+bool keep_going = true;
+const int max_connections = 10;
+
+struct MyException
+{
+	MyException(string f, int l, string m) : function(string(f)), line(l), msg(m) {};
+	string function;
+	int line;
+	string msg;
+};
+
+#define MYEXCEPTION(m)		MyException(string(__FUNCTION__), __LINE__, string(m))
+
+void SIGINTHandler(int)
+{
+    keep_going = false;
+}
+
+void ConnectionHandler(sockaddr_in client_info, int socket, int cid)
+{
+	size_t length;
+	size_t bytes_read;
+	string s;
+
+	try
+	{
+		while (true) {
+			bytes_read = recv(socket, (void *) &length, sizeof(length), 0);
+			if (bytes_read != sizeof(length))
+				throw MYEXCEPTION("");
+
+			length = ntohl(length);
+			s.resize(length);
+
+			bytes_read = recv(socket, (void *) &s[0], length, 0);
+			if (bytes_read != length)
+				throw MYEXCEPTION("");
+
+			cout << "Conn[" << cid << "]: " << s << endl;
+			if (s == "quit")
+				throw MYEXCEPTION("");
+		}
+	}
+	catch (MyException e)
+	{
+		cerr << e.function << " " << e.line << " " << e.msg << endl;		
+	}
+
+	close(socket);
+}
+
+void AcceptConnections(int port)
+{
+	vector<thread *> threads;
+
+	// By default, Linux apparently retries  interrupted system calls.  This defeats the
+	// purpose of interrupting them, doesn't it? The call to siginterrupt disables this.
+	signal(SIGINT, SIGINTHandler);
+	siginterrupt(SIGINT, 1);
+	int incoming_socket = -1;
+	int listening_socket = -1;
+
+	try 
+	{
+		listening_socket = socket(AF_INET, SOCK_STREAM, 0);
+		if (listening_socket < 0) {
+			throw MYEXCEPTION("opening listening socket failed " + string(strerror(errno)));
+		}
+
+		int optval = 1;
+		optval = setsockopt(listening_socket, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval);
+		if (optval < 0) {
+			throw MYEXCEPTION("setting socket opt failed " + string(strerror(errno)));
+		}
+
+		sockaddr_in listening_sockaddr;
+
+		memset(&listening_sockaddr, 0, sizeof(sockaddr_in));
+		listening_sockaddr.sin_family = AF_INET;
+		listening_sockaddr.sin_port = htons(port);
+		listening_sockaddr.sin_addr.s_addr = INADDR_ANY;
+
+		if (bind(listening_socket, (sockaddr *) &listening_sockaddr, sizeof(sockaddr_in)) < 0) {
+			throw MYEXCEPTION("bind failed " + string(strerror(errno)));
+		}
+
+		if (listen(listening_socket , max_connections) != 0) {
+			throw MYEXCEPTION("listen failed " + string(strerror(errno)));
+		}
+
+		sockaddr_in client_info;
+		memset(&client_info, 0, sizeof(sockaddr_in));
+		int c = sizeof(sockaddr_in);
+
+		int connection_counter = 0;
+
+		while ((incoming_socket = accept(listening_socket, (sockaddr *) &client_info, (socklen_t *) &c)) > 0) {
+			if (!keep_going)
+				break;
+
+			thread * t = new thread(ConnectionHandler, client_info, incoming_socket, connection_counter++);
+			threads.push_back(t);
+		}
+	}
+	catch (MyException e) {
+		cerr << e.function << " " << e.line << " " << e.msg << endl;
+	}
+
+	if (incoming_socket >= 0)
+		close(incoming_socket);
+
+	if (listening_socket >= 0)
+		close(listening_socket);
+
+	for (auto it = threads.begin(); it < threads.end(); it++) {
+		if (*it != nullptr) {
+			(*it)->join();
+		}
+	}
+}
+
+int main(int argc, char * argv[])
+{
+	int port = 5077;
+	int opt;
+
+	while ((opt = getopt(argc, argv, "p:")) >= 0) {
+		switch (opt) {
+			case 'p':
+				port = atoi(optarg);
+				break;
+
+			default:
+				break;
+		}
+	}
+	AcceptConnections(port);
+	return 0;
+}
+
